79d2e09980287dabe550f269def99b141992a84d
darky74 <julien.fourdachon@gmail.com>
2018-05-25 09:19:12 +0000

    changement nom de table user

--- src/main/java/com/example/test/User.java
+++ src/main/java/com/example/test/User.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
 @Entity
+@Table(name = "user_social")
 public class User {
 	
 	@Id
